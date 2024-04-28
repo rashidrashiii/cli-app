866db316e4a1150c2a6e5168ffd932acd5492f23
Muhammed Rashid V <87640868+rashidrashiii@users.noreply.github.com>
2024-04-28 21:57:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import click
 import http.server
 import socketserver
 import webbrowser
+import psutil
 
 
 # HTML code
@@ -47,6 +48,19 @@ HTML_CODE = """
         <dialog id="loading">
             <h1>Loading...</h1>
         </dialog>
+        <div id="router-outlet">
+        </div>
+        <script type="py" src="./main.py" config="./pyscript.toml"></script>
+        <script src="./assets/scripts/script.js"></script>
+        <script src="./assets/scripts/routing-script.js"></script>
+    </body>
+</html>
+"""
+
+#APP HTML CODE
+APP_HTML_CODE = """
+<header-component></header-component>
+
 
         <nav class="navbar" style="background-color: #000000">
             <div class="app-header">
@@ -56,7 +70,11 @@ HTML_CODE = """
                 <a class="title" href="" style="color: #f0ab3c">Todo App</a>
             </div>
         </nav>
-
+        <button onclick="navigateToPage('footer')">Go to Footer Page</button>
+        <div id="content">
+            <!-- Content will be loaded here -->
+        </div>
+    
         <section class="pyscript">
             <main>
                 <section>
@@ -83,11 +101,15 @@ HTML_CODE = """
                 </section>
             </main>
 
-            <script type="py" src="./main.py" config="./pyscript.toml"></script>
+
         </section>
-    </body>
-</html>
 """
+
+#APP python CODE
+APP_PYTHON_CODE = """
+
+"""
+
 
 # Python code
 PYTHON_CODE = """
@@ -221,12 +243,118 @@ name = "Todo App"
 description = "A simple To Do application written in PyScript."
 """
 
-warning_comment = """
+WARNING_COMMENT = """
 /**
  * WARNING: Do not remove the commented annotations above and below the classes.
  * They are used by the script to identify class definitions.
  */
 """
+
+
+ROUTING_WARNING_COMMENT = """
+/**
+ * WARNING: Do not remove the commented annotations above and below the classes.
+ * They are used by the script to identify class definitions.
+ */
+
+var isProduction = false;
+const initialURL = window.location.href;
+var routeBaseUrl = isProduction ? initialURL : '';
+
+// Function to load HTML file based on URL parameter
+function loadPage(page) {
+    // Load the specified HTML file
+    fetch(page) // Assuming components folder is in the same directory as index.html
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('router-outlet').innerHTML = html;
+        })
+        .catch(error => console.error('Error loading content: ', error));
+}
+
+// Function to navigate to a specific page when button is clicked
+function navigateToPage(pageId) {
+
+    getPageInfo(pageId)
+        .then(routeInfo => {
+            console.log('routeInfo-routeInfo',routeInfo);
+            // Call another function and pass routeInfo as an argument
+            routeToPage(routeInfo);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    
+}
+
+function routeToPage(page) {
+    console.log('page-page-page',page);
+    // Update the URL with the page parameter
+    window.history.pushState({}, routeBaseUrl, `/${page.url}`);
+    // Load the page content
+
+    var pageUrl = isProduction ? `${routeBaseUrl}${page.path}` : `${page.path}`;
+    loadPage(pageUrl);
+}
+
+function getPageInfo(pageId) {
+    return new Promise((resolve, reject) => {
+      fetch('../../router/router.json')
+        .then(response => response.json())
+        .then(data => {
+          // Assuming your JSON structure looks like { "routes": [ { ... }, { ... } ] }
+          const routes = data.routes;
+  
+          // Find the route object with the specified id
+          const foundRoute = routes.find(route => route.id === pageId);
+  
+          if (foundRoute) {
+            // Resolve the promise with the found route object
+            resolve(foundRoute);
+          } else {
+            // Reject the promise if route is not found
+            reject('Route not found');
+          }
+        })
+        .catch(error => {
+          // Reject the promise if there's an error fetching JSON file
+          reject('Error fetching JSON file: ' + error);
+        });
+    });
+  }
+
+// Function to load HTML file based on URL parameter
+function loadPageFromURL() {
+    // Get the URL parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log('initialURL-initialURL',initialURL);
+    console.log('routeBaseUrl',routeBaseUrl);
+    window.history.pushState({}, routeBaseUrl);
+    // if (page) {
+    //     // Load the specified HTML file
+    //     loadPage(page);
+    // }
+}
+
+// Call the function when the page loads
+window.onload = loadPageFromURL;
+window.onload = navigateToPage('');
+// Listen for changes in the URL
+window.addEventListener('popstate', loadPageFromURL);
+"""
+
+ROUTER_JSON = """
+{
+    "routes" : [
+        {
+            "path" : "app/app.component.html",
+            "url" : "",
+            "id" : ""
+        }
+    ]   
+}
+"""
+
 
 def find_html_files(directory):
     html_files = []
@@ -239,10 +367,13 @@ def find_html_files(directory):
 def generate_custom_classes(html_files):
     custom_classes = {}
     for html_file in html_files:
-        class_name = os.path.splitext(os.path.basename(html_file))[0].title().replace('_', '') + 'Component'
-        with open(html_file, 'r') as file:
-            html_content = file.read()
-        custom_classes[class_name.lower()] = f"""\
+        file_name = os.path.splitext(os.path.basename(html_file))[0]
+        # Remove dots from the file name
+        class_name = file_name.title().replace('_', '').replace('.', '') + 'Component'
+        if class_name!='IndexComponent':
+            with open(html_file, 'r') as file:
+                html_content = file.read()
+                custom_classes[class_name.lower()] = f"""\
 class {class_name} extends HTMLElement {{
     constructor() {{
         super();
@@ -294,6 +425,18 @@ def generate(name):
     assets_path = os.path.join(folder_name, "assets")
     os.makedirs(assets_path, exist_ok=True)
 
+    # Create router folder
+    router_path = os.path.join(folder_name, "router")
+    os.makedirs(router_path, exist_ok=True)
+
+    # Create scripts folder
+    scripts_path = os.path.join(assets_path, "scripts")
+    os.makedirs(scripts_path, exist_ok=True)
+
+    # Create app folder
+    app_path = os.path.join(folder_name, "app")
+    os.makedirs(app_path, exist_ok=True)
+
     # Write HTML code to index.html
     with open(os.path.join(folder_name, "index.html"), "w", encoding="utf-8") as f:
         f.write(HTML_CODE)
@@ -303,12 +446,28 @@ def generate(name):
         f.write(PYTHON_CODE)
 
     # Write javascript code to index.html
-    with open(os.path.join(folder_name, "script.html"), "w", encoding="utf-8") as f:
-        f.write(warning_comment)
+    with open(os.path.join(scripts_path, "script.js"), "w", encoding="utf-8") as f:
+        f.write(WARNING_COMMENT)
+
+    # Write javascript code to index.html
+    with open(os.path.join(scripts_path, "routing-script.js"), "w", encoding="utf-8") as f:
+        f.write(ROUTING_WARNING_COMMENT)
 
     # Write Pyscript.toml code to main.py
     with open(os.path.join(folder_name, "pyscript.toml"), "w", encoding="utf-8") as f:
         f.write(PYSCRIPTTOML)
+
+    # Write ROUTER code to router.json
+    with open(os.path.join(router_path, "router.json"), "w", encoding="utf-8") as f:
+        f.write(ROUTER_JSON)
+
+    # Write App html code to app folder
+    with open(os.path.join(app_path, "app.component.html"), "w", encoding="utf-8") as f:
+        f.write(APP_HTML_CODE)
+
+    # Write App python code to app folder
+    with open(os.path.join(app_path, "app.component.py"), "w", encoding="utf-8") as f:
+        f.write(APP_PYTHON_CODE)
 
     # Write example.css
     css_folder_path = os.path.join(assets_path, "css")
@@ -330,9 +489,11 @@ def generate(name):
     print("Project created successfully.")
 
 @cli.command()
-def run(port=3000):
+def run(start_port=3000, max_attempts=10):
     project_directory = os.getcwd()
-    script_file = os.path.join(project_directory, 'script.js')
+    assets_path = os.path.join(project_directory, "assets")
+    scripts_path = os.path.join(assets_path, "scripts")
+    script_file = os.path.join(scripts_path, 'script.js')
     html_files = find_html_files(project_directory)
 
     custom_classes = generate_custom_classes(html_files)
@@ -351,10 +512,97 @@ def run(port=3000):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, directory=project_directory, **kwargs)
 
-    with socketserver.TCPServer(("", port), Handler) as httpd:
-        print("Server started at port", port)
-        webbrowser.open(f'http://localhost:{port}/index.html')
-        httpd.serve_forever()
+    # Attempt to find an available port
+    for port in range(start_port, start_port + max_attempts):
+        try:
+            # Check if the port is already in use
+            if is_port_in_use(port):
+                print(f"Port {port} is already in use. Attempting to kill the port and start the server...")
+                continue
+
+            # Start the server
+            with socketserver.TCPServer(("", port), Handler) as httpd:
+                print("Server started at port", port)
+                webbrowser.open(f'http://localhost:{port}/index.html')
+                httpd.serve_forever()
+                # If the server starts successfully, exit the loop
+                break
+        except OSError as e:
+            if e.errno == 48:  # Address already in use
+                print(f"Port {port} is already in use. Trying next port...")
+                continue
+            else:
+                raise
+    else:
+        print("Unable to find an available port. Exiting.")
+
+@cli.command()
+def build(start_port=3000, max_attempts=10):
+    project_directory = os.getcwd()
+    assets_path = os.path.join(project_directory, "assets")
+    scripts_path = os.path.join(assets_path, "scripts")
+    script_file = os.path.join(scripts_path, 'routing-script.js')
+    setProduction(script_file)
+
+    index_file = os.path.join(project_directory, 'index.html')
+    
+    if not os.path.exists(index_file):
+        print("Error: index.html file not found in the root directory.")
+        return
+    
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=project_directory, **kwargs)
+
+    # Attempt to find an available port
+    for port in range(start_port, start_port + max_attempts):
+        try:
+            # Check if the port is already in use
+            if is_port_in_use(port):
+                print(f"Port {port} is already in use. Attempting to kill the port and start the server...")
+                continue
+
+            # Start the server
+            with socketserver.TCPServer(("", port), Handler) as httpd:
+                print("Server started at port", port)
+                webbrowser.open(f'http://localhost:{port}/index.html')
+                httpd.serve_forever()
+                # If the server starts successfully, exit the loop
+                break
+        except OSError as e:
+            if e.errno == 48:  # Address already in use
+                print(f"Port {port} is already in use. Trying next port...")
+                continue
+            else:
+                raise
+    else:
+        print("Unable to find an available port. Exiting.")
+
+
+def is_port_in_use(port):
+    """
+    Check if the given port is already in use by any process.
+    """
+    for conn in psutil.net_connections():
+        if conn.laddr.port == port:
+            return True
+    return False
+
+def setProduction(script_path):
+        # Open the JavaScript file
+    with open(script_path, 'r') as file:
+        lines = file.readlines()
+
+    # Find the line with the variable declaration
+    for i, line in enumerate(lines):
+        if 'var isProudction = false;' in line:
+            # Replace the value with true
+            lines[i] = 'var isProduction = true;\n'
+            break
+
+    # Write the modified content back to the file
+    with open(script_path, 'w') as file:
+        file.writelines(lines)
 
 
 if __name__ == '__main__':
